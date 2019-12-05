04bf296a8d6f17607feec389a5a1889b8173a78d
Kai Peterson <pete9372@umn.edu>
2019-12-05 11:18:57 -0600

    setup app and complete pet form component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// component imports
+import PetForm from './PetForm/PetForm';
+
 class App extends Component {
   state = {
     mode: false,
@@ -18,6 +21,25 @@ class App extends Component {
         <header className="App-header">
           <h1>Pet Hotel</h1>
         </header>
+        <div className="content-container">
+          <div className="nav-buttons">
+            <button onClick={this.handleClick}>Dashboard</button>
+            <button onClick={this.handleClick}>Manage Owners</button>
+          </div>
+          {!this.state.mode &&
+            <>
+              <PetForm />
+              {/* pet form component here
+          pet table component here */}
+            </>
+          }
+          {this.state.mode &&
+            <>
+              {/* owner form component here
+          owner table component here */}
+            </>
+          }
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,15 +6,25 @@ import {createStore, combineReducers,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import logger from 'redux-logger';
 import createSagaMiddleWare from 'redux-saga';
-import {takeEvery, put} from 'redux-saga/effects';
+import {takeEvery, takeLatest, put} from 'redux-saga/effects';
 
 function* rootSaga() {
     yield takeEvery('GET_PETS',petsSaga);
+    yield takeLatest('ADD_PET', addPetSaga);
 }
 
 function* petsSaga(action) {
     // axios req to get data
     // call 'SET_PET_INFO' reducer
+}
+
+function* addPetSaga(action) {
+    try {
+        console.log(action.payload);
+    }
+    catch (error) {
+        console.log('error in addPetSaga', error);  
+    }
 }
 
 const sagaMiddleware = createSagaMiddleWare();
