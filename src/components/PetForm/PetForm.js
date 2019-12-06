06d9893bb36951ef71287504ad81c2cae0397662
Kai Peterson <pete9372@umn.edu>
2019-12-06 10:39:07 -0600

    owner dropdown populating, count on dom, owner name on dom

--- src/components/PetForm/PetForm.js
+++ src/components/PetForm/PetForm.js
@@ -8,8 +8,20 @@ export class PetForm extends Component {
             name: '',
             color: '',
             breed: '',
-            owner: '',
+            owner_id: '',
         }
+    }
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_OWNERS' })
+        setTimeout(() => {
+            this.setState({
+                pet: {
+                    ...this.state.pet,
+                    owner_id: this.props.ownersReducer[0].id
+                }
+            })
+        }, 2000)
     }
 
     handleChange = (prop, event) => {
@@ -22,25 +34,32 @@ export class PetForm extends Component {
     }
 
     handleClick = () => {
-        this.props.dispatch({type: 'ADD_PET', payload: this.state.pet})
+        this.props.dispatch({ type: 'ADD_PET', payload: this.state.pet })
     }
 
     render() {
         return (
             <>
-            <h2>Add Pet</h2>
-            <div className="pet-form-inputs">
-                <input onChange={(event) => this.handleChange('name', event)} placeholder="Pet Name"/>
-                <input onChange={(event) => this.handleChange('color', event)} placeholder="Pet Color"/>
-                <input onChange={(event) => this.handleChange('breed', event)} placeholder="Pet Breed"/>
-                <select>
-                    {/* map through owner names from DB to populate <option></option>'s */}
-                </select>
-                <button onClick={this.handleClick}>Submit</button>
-            </div>
+                <h2>Add Pet</h2>
+                <div className="pet-form-inputs">
+                    <input onChange={(event) => this.handleChange('name', event)} placeholder="Pet Name" />
+                    <input onChange={(event) => this.handleChange('color', event)} placeholder="Pet Color" />
+                    <input onChange={(event) => this.handleChange('breed', event)} placeholder="Pet Breed" />
+                    <select onClick={(event) => this.handleChange('owner_id', event)}>
+                        {/* map through owner names from DB to populate <option></option>'s */}
+                        {this.props.ownersReducer.map((owner) =>
+                            <option value={owner.id}>{owner.name}</option>
+                        )}
+                    </select>
+                    <button onClick={this.handleClick}>Submit</button>
+                </div>
             </>
         )
     }
 }
 
-export default connect()(PetForm);
+const mapReduxStateToProps = state => {
+    return state;
+}
+
+export default connect(mapReduxStateToProps)(PetForm);
